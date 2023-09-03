8854f4c0235059c63b0c8e46ac7b4c313d0a3dff
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-09-04 01:57:18 +0900

    master

--- monolithic-1/function_app.py
+++ monolithic-1/function_app.py
@@ -2,22 +2,20 @@ import azure.functions as func
 import logging
 from collections import Counter
 import math
-import copy
 import random
 import time
-del_count = 10 # 削除するマスの数を設定
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # パラメータ取得関数
-def get_param_from_request(req, param_name, json_key=None):
-    row = req.params.get(param_name)
-    if row is None:
+def get_param_from_request(req, param_name, json_key=None) -> int:
+    param = req.params.get(param_name)
+    if param is None:
         try:
-            row = req.get_json().get(json_key or param_name)
+            param = req.get_json().get(json_key or param_name)
         except (ValueError, KeyError):
-            row = 1
-    return int(row)
+            param = 0
+    return int(param)
 
 ##################
 ## クライアント ##
@@ -28,9 +26,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # パラメータかjsonから値取得
     row = get_param_from_request(req, "row")
+    del_num = get_param_from_request(req, "del")
 
     # 関数呼び出し
-    result = execute_process(row)
+    result = execute_process(row, del_num)
 
     return func.HttpResponse(result)
 
@@ -38,122 +37,104 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 ##############
 ## 実行部分 ##
 ##############
-def execute_process(row_count: int) -> str:
-    # 解けない問題であれば繰り返し生成 -> 解く
+def execute_process(size: int, del_num: int) -> str:
     start = time.time()   # 実行開始時間保存
-    data = prepare_block_check(row_count)   # 対角線上に数字が含まれているパネル(data)を作成
-    repeate = 1   # 問題を生成した回数
-    solver(data, 0, 0, row_count)   # 対角線上に数字が含まれているパネル(data)を作成
-    while any(0 in row for row in data):
-        data = prepare_block_check(row_count)
+    repeate = 0   # 問題を生成した回数
+    while True:  # 解けない問題であれば繰り返し生成 -> 解く
+        data = prepare_block_check(size)
         repeate += 1
-        solver(data, 0, 0, row_count)
+        if solver(data, size):
+            break
     performance_time = time.time() - start
 
-    result = []  # 結果の文字列を格納するリスト
-    result.append("数独を生成した回数：" + str(repeate) + "\n")
-    result.append("実行時間 : " + str(performance_time) + "\n")
+    result = []  # 画面出力を格納するリスト
+    result = [f"数独を生成した回数：{repeate}\n", f"実行時間 : {performance_time}\n"]
     for a in data:
         result.append(" ".join(map(str, a)))
     result.append("")  # 空行を追加
 
-    draw_out(data, del_count, row_count)  # 要素を削除
+    draw_out(data, del_num, size)  # 要素を削除
     result.append("生成した例題")
     for a in data:
         result.append(" ".join(map(str, a)))
 
     return "\n".join(result)  # 改行を挟んでリストを文字列に変換して返す
 
+
 #########################
 ##  アクティビティ関数  ##
 #########################
-# 重複なしのランダムを生成し、リストで返す
-def random_int(a: int, b: int, k: int) -> list:
-    ns = []
-    while len(ns) < k:
-        n = random.randint(a, b)
-        if n not in ns:
-            ns.append(n)
-    return ns
-
 # 対角線上のブロックにランダムな数字格納
-def prepare_block_check(row_count: int) -> list:
-  panel = [[0 for i in range(row_count)] for j in range(row_count)] # 数独用のパネル
-  random = []   # 乱数を格納
-  mass = int(math.sqrt(row_count))
+def prepare_block_check(size: int) -> list:
+  panel = [[0 for i in range(size)] for j in range(size)] # 数独用のパネル
+  
+  mass = int(math.sqrt(size))
   for i in range(mass):
-    xbase = mass * i   # 対角線上の開始点が panel[ybase][xbase]となる
-    ybase = mass * i
-    random = random_int(1, row_count, row_count)
+    xbase, ybase = mass * i, mass * i   # 対角線上の開始点が panel[ybase][xbase]となる
+    numbers = list(range(1, size + 1))
+    random.shuffle(numbers)
+
     for y in range(ybase, ybase + mass):
       for x in range(xbase, xbase + mass):
-        panel[y][x] = random[(y - ybase)*mass + (x - xbase)]
+        panel[y][x] = numbers.pop(0)
   return panel
 
 # 横をチェック
-def row_check(values: list, y: int, i: int, row_count: int) -> bool:
-    return all(True if i != values[y][_x] else False for _x in range(row_count))
+def row_check(values: list, y: int, i: int, size: int) -> bool:
+    return all(i != values[y][_x] for _x in range(size))
 
 # 縦をチェック
-def column_check(values: list, x: int, i: int, row_count: int) -> bool:    
-    return all(True if i != values[_y][x] else False for _y in range(row_count))
+def column_check(values: list, x: int, i: int, size: int) -> bool:    
+    return all(i != values[_y][x] for _y in range(size))
 
 # row x rowのブロックをチェック
-def block_check(values: list, x: int, y: int, i: int, row_count: int) -> bool:
-    mass = int(math.sqrt(row_count))
-    xbase = (x // mass) * mass
-    ybase = (y // mass) * mass
-    return all(True if i != values[_y][_x] else False
+def block_check(values: list, x: int, y: int, i: int, size: int) -> bool:
+    mass = int(math.sqrt(size))
+    xbase, ybase = (x // mass) * mass,  (y // mass) * mass
+    return all(i != values[_y][_x]
             for _y in range(ybase, ybase + mass)
                 for _x in range(xbase, xbase + mass))
 
 # すべてのチェックを満たしているかチェック
-def check(values: list, x: int, y: int, i: int, row_count: int) -> bool:
-    return all([row_check(values, y, i, row_count), 
-                column_check(values, x, i, row_count), 
-                block_check(values, x, y, i, row_count)])
+def check(values: list, x: int, y: int, i: int, size: int) -> bool:
+    return all([row_check(values, y, i, size), 
+                column_check(values, x, i, size), 
+                block_check(values, x, y, i, size)])
 
 # 入れられる数の少ないリスト番号を返す
-def count(values: list, row_count: int) -> tuple:
-  min_val = row_count
-  min_x, min_y = -1, -1   # 候補の個数が少ない座標を保存
-  result = []   # 入る候補の数字を保存
+def count(values: list, size: int) -> tuple:
+  min_candidates = size
+  min_x, min_y, result = -1, -1, []   # 候補の個数が少ない座標を保存, 入る候補の数字を保存
 
-  for y in range(row_count):
-    for x in range(row_count):
+  for y in range(size):
+    for x in range(size):
       if values[y][x] == 0:  # マスが0のとき
-        panel = []
-        for i in range(1, row_count+1):
-          if check(values, x, y, i, row_count): # 入れられる数字を追加
-            panel.append(i)
-        if (min_val > len(panel)): # 入れられる候補の少ないものが見つかったら更新
-          min_val = len(panel)
-          result = copy.copy(panel)
-          min_x, min_y = x, y
-
+        panel = [i for i in range(1, size+1) if check(values, x, y, i, size)]  # 入れられる数字を追加
+        if len(panel) < min_candidates: # 入れられる候補の少ないものが見つかったら更新
+            min_candidates, min_x, min_y, result = len(panel), x, y, panel
   return min_x, min_y, result
 
 # 数独を解く関数
-def solver(values: list, x: int, y: int, row_count: int) -> bool:
-  min_x, min_y, panel = count(values, row_count)  # 入れられる数の少ないインデックス取得
+def solver(values: list, size: int) -> bool:
+    min_x, min_y, panel = count(values, size)  # 入れられる数の少ないインデックス取得
 
-  if (min_x==-1): #終了条件
-    return True
-  for i in range(0, len(panel)):
-    values[min_y][min_x] = panel[i]  #数字を入れる
-    if solver(values, min_x, min_y, row_count):
-      return True
-    values[min_y][min_x] = 0 #戻ってきたら0に戻す    
+    if min_x == -1: #終了条件
+        return True
+    for i in range(len(panel)):
+        values[min_y][min_x] = panel[i]  #数字を入れる
+        if solver(values, size):
+            return True
+        values[min_y][min_x] = 0 #戻ってきたら0に戻す    
 
-  return False
+    return False
 
 # 完成している数独から要素を削除する
-def draw_out(values: list, del_count: int, row_count: int) -> list:
-    draw_out_index = []  
-    draw_out_index = random_int(0, row_count * row_count - 1, del_count)  # 抜き出す配列の要素番号を取得
+def draw_out(values: list, del_count: int, size: int) -> list:
+    indices = list(range(size * size))  # すべての要素のインデックスのリストを作成
+    random.shuffle(indices)  # インデックスをシャッフル
     for i in range(del_count):
-        y = draw_out_index[i] // row_count
-        x = draw_out_index[i] % row_count
+        index = indices[i]
+        y = index // size
+        x = index % size
         values[y][x] = 0
     return values
-
--- monolithic-2/function_app.py
+++ monolithic-2/function_app.py
@@ -1,21 +1,21 @@
 import azure.functions as func
 import logging
-import time
 from collections import Counter
+import math
+import random
+import time
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-activity_functions = {"failed", "sleep", "replace", "sort", "count_up", "delete"}
-SLEEP_TIMES = 10
-JOIN_TIMES = 10
 
-def get_param_from_request(req, param_name, json_key=None):
+# パラメータ取得関数
+def get_param_from_request(req, param_name, json_key=None) -> int:
     param = req.params.get(param_name)
     if param is None:
         try:
             param = req.get_json().get(json_key or param_name)
         except (ValueError, KeyError):
-            pass
-    return param
+            param = 0
+    return int(param)
 
 ##################
 ## クライアント ##
@@ -25,80 +25,116 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # パラメータかjsonから値取得
-    process = get_param_from_request(req, "process")
-    if process not in activity_functions:
-        process = "failed"
-    char = get_param_from_request(req, "char")
-    string = get_param_from_request(req, "string")
+    row = get_param_from_request(req, "row")
+    del_num = get_param_from_request(req, "del")
 
     # 関数呼び出し
-    result = execute_process(process, char, string)
+    result = execute_process(row, del_num)
 
     return func.HttpResponse(result)
 
-#########################
-## オーケストレーション ##
-#########################
-def execute_process(process, char, string):
-    if process in {"failed", "sleep"}:
-        result = globals()[process]()
-    else:
-        strings = join(string)
-        inputs = {"char": char, "strings": strings}
-        result = globals()[process](inputs)
 
-    return result
+##############
+## 実行部分 ##
+##############
+def execute_process(size: int, del_num: int) -> str:
+    start = time.time()   # 実行開始時間保存
+    repeate = 0   # 問題を生成した回数
+    while True:  # 解けない問題であれば繰り返し生成 -> 解く
+        data = prepare_block_check(size)
+        repeate += 1
+        if solver(data, size):
+            break
+    performance_time = time.time() - start
+
+    result = []  # 画面出力を格納するリスト
+    result = [f"数独を生成した回数：{repeate}\n", f"実行時間 : {performance_time}\n"]
+    for a in data:
+        result.append(" ".join(map(str, a)))
+    result.append("")  # 空行を追加
+
+    draw_out(data, del_num, size)  # 要素を削除
+    result.append("生成した例題")
+    for a in data:
+        result.append(" ".join(map(str, a)))
+
+    return "\n".join(result)  # 改行を挟んでリストを文字列に変換して返す
 
 
 #########################
 ##  アクティビティ関数  ##
 #########################
-def failed():
-    return "failed_function executed successfully."
+# 対角線上のブロックにランダムな数字格納
+def prepare_block_check(size: int) -> list:
+  panel = [[0 for i in range(size)] for j in range(size)] # 数独用のパネル
+  
+  mass = int(math.sqrt(size))
+  for i in range(mass):
+    xbase, ybase = mass * i, mass * i   # 対角線上の開始点が panel[ybase][xbase]となる
+    numbers = list(range(1, size + 1))
+    random.shuffle(numbers)
 
-def sleep():
-        time.sleep(SLEEP_TIMES)
-        return f"It was stopped for {SLEEP_TIMES} seconds."
+    for y in range(ybase, ybase + mass):
+      for x in range(xbase, xbase + mass):
+        panel[y][x] = numbers.pop(0)
+  return panel
 
-# 文字列長変更
-def join(string):
-    strings = " ".join([string] * JOIN_TIMES)
-    return strings
+# 横をチェック
+def row_check(values: list, y: int, i: int, size: int) -> bool:
+    return all(i != values[y][_x] for _x in range(size))
 
-#charを,設定した文字列に置換 (if文でさらに細分化し文字列を設定)
-def replace(inputs):
-    char = inputs["char"]
-    strings = inputs["strings"]
+# 縦をチェック
+def column_check(values: list, x: int, i: int, size: int) -> bool:    
+    return all(i != values[_y][x] for _y in range(size))
 
-    if 'a' <= char[0] <= 'i':
-        replacement = 'test1'
-    elif 'j' <= char[0] <= 's':
-        replacement = 'test2'
-    else:
-        replacement = 'test3'
+# row x rowのブロックをチェック
+def block_check(values: list, x: int, y: int, i: int, size: int) -> bool:
+    mass = int(math.sqrt(size))
+    xbase, ybase = (x // mass) * mass,  (y // mass) * mass
+    return all(i != values[_y][_x]
+            for _y in range(ybase, ybase + mass)
+                for _x in range(xbase, xbase + mass))
 
-    strings = strings.replace(char, replacement)
-    return f"The character {char} was converted => {strings}."
+# すべてのチェックを満たしているかチェック
+def check(values: list, x: int, y: int, i: int, size: int) -> bool:
+    return all([row_check(values, y, i, size), 
+                column_check(values, x, i, size), 
+                block_check(values, x, y, i, size)])
 
-#charを削除        
-def delete(inputs):
-    char = inputs["char"]
-    strings = inputs["strings"]
-    strings = strings.replace(char, '')
-    return f"The character {char} was delated => {strings}."
-    
-#charの出現回数
-def count_up(inputs):
-    char = inputs["char"]
-    strings = inputs["strings"]
-    char_count = strings.count(char)
-    return f"The character '{char}' appears {char_count} times."
-    
-#回数ソート
-def sort(inputs):
-        strings = inputs["strings"]
-        words = strings.split()
-        word_counts = Counter(words)
-        sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
-        result = '\n'.join([f"{word}: {count}" for word, count in sorted_word_counts])
-        return result
+# 入れられる数の少ないリスト番号を返す
+def count(values: list, size: int) -> tuple:
+  min_candidates = size
+  min_x, min_y, result = -1, -1, []   # 候補の個数が少ない座標を保存, 入る候補の数字を保存
+
+  for y in range(size):
+    for x in range(size):
+      if values[y][x] == 0:  # マスが0のとき
+        panel = [i for i in range(1, size+1) if check(values, x, y, i, size)]  # 入れられる数字を追加
+        if len(panel) < min_candidates: # 入れられる候補の少ないものが見つかったら更新
+            min_candidates, min_x, min_y, result = len(panel), x, y, panel
+  return min_x, min_y, result
+
+# 数独を解く関数
+def solver(values: list, size: int) -> bool:
+    min_x, min_y, panel = count(values, size)  # 入れられる数の少ないインデックス取得
+
+    if min_x == -1: #終了条件
+        return True
+    for i in range(len(panel)):
+        values[min_y][min_x] = panel[i]  #数字を入れる
+        if solver(values, size):
+            return True
+        values[min_y][min_x] = 0 #戻ってきたら0に戻す    
+
+    return False
+
+# 完成している数独から要素を削除する
+def draw_out(values: list, del_count: int, size: int) -> list:
+    indices = list(range(size * size))  # すべての要素のインデックスのリストを作成
+    random.shuffle(indices)  # インデックスをシャッフル
+    for i in range(del_count):
+        index = indices[i]
+        y = index // size
+        x = index % size
+        values[y][x] = 0
+    return values
