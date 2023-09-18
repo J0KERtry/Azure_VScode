cf0ac410fab5a17a0e673d99ab781115f5d6782a
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-09-18 15:06:47 +0900

    master

---
+++ time1/function_app.py
@@ -0,0 +1,42 @@
+### データの転送 ###
+# size1にコピー済み
+
+import azure.functions as func
+import azure.durable_functions as df
+import logging
+
+app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="orchestrators/client_function")
+@app.durable_client_input(client_name="client")
+async def client_function(req: func.HttpRequest, client: df.DurableOrchestrationClient) -> func.HttpResponse:
+
+    # オーケストレーションの起動
+    instance_id = await client.start_new("orchestrator", None, {})
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    
+    # オーケストレーションの完了を待機
+    await client.wait_for_completion_or_create_check_status_response(req, instance_id)
+
+    # オーケストレーションの実行状態を取得
+    status = await client.get_status(instance_id)
+
+    # オーケストレーションの実行結果を取得
+    runtime = status.runtime_status
+    input_ = status.input_
+    output = status.output
+    return f"runtime: {runtime}\n\ninput_:{input_}\n\noutput:{output}" 
+
+
+@app.orchestration_trigger(context_name="context")
+def orchestrator(context: df.DurableOrchestrationContext) -> str:
+    result = yield context.call_activity("activity1", "")
+    return result
+
+
+@app.activity_trigger(input_name="blank")
+def activity1(blank: str):
+    return " end "
+
+data_1kb = bytearray(1024)  # 1KBのデータを生成
+data_1mb = bytearray(1024 * 1024)  # 1MBのデータを生成
+data_1gb = bytearray(1024 * 1024 * 1024)  # 1GBのデータを生成
