40f4ed277bd46a10bb4722fe3dc8610af9cba9ef
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-10-14 15:36:26 +0900

    master

--- size-time/function_app.py
+++ size-time/function_app.py
@@ -11,9 +11,10 @@ app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="orchestrators/client_function")
 @app.durable_client_input(client_name="client")
 async def client_function(req: func.HttpRequest, client: df.DurableOrchestrationClient) -> func.HttpResponse:
+    activity = int(req.params.get('activity') or req.get_json().get('activity'))
+    size = int(req.params.get('size') or req.get_json().get('size'))
 
-    # オーケストレーションの起動
-    instance_id = await client.start_new("orchestrator", None, {})
+    instance_id = await client.start_new("orchestrator", None, {"activity":activity, "size":size})
     logging.info(f"Started orchestration with ID = '{instance_id}'.")
     
     # オーケストレーションの完了を待機
@@ -26,16 +27,42 @@ async def client_function(req: func.HttpRequest, client: df.DurableOrchestration
 
 @app.orchestration_trigger(context_name="context")
 def  orchestrator(context: df.DurableOrchestrationContext):
-    start  =  time.time()
-    result  =  yield context.call_activity("activity1", "")
-    transfer_time  =  time.time() - start
-    return  {"transfer_time" : transfer_time}
+    parameter = context.get_input()
+    activity = int(parameter.get("activity"))
+    size = int(parameter.get("size"))
+    
+    if activity == 1:
+        result  =  yield context.call_activity("activity1", "size")
+        start = result("start")
+        transfer_time  =  time.time() - start
+        return  {"transfer_time" : transfer_time}
+    
+    elif activity == 2:
+        data = np.random.rand(size) 
+        df  =  pd.DataFrame(data) # DataFrame作成
+        df_ = df.to_dict() # DataFrameをシリアライズ可能なdictに変換
+        start  =  time.time()
+        result  =  yield context.call_activity("activity2", "start")
+        transfer_time = result("transfer_time")
+        return  {"transfer_time" : transfer_time}
 
 
+# sizeからDataFrame作成し、アクティビティ関数 -> オーケストレーター関数に転送
 @app.activity_trigger(input_name="blank")
-def  activity1(blank: str) -> str:
-    # 1文字8バイトのfloat64。n個のデータを生成した場合、サイズは8nバイト. 8n/(1024*1024)MB
-    data = np.random.rand(100000)  
+def  activity1(context: df.DurableOrchestrationContext) -> str:
+    parameter = context.get_input()
+    size = int(parameter.get("size"))
+
+    data = np.random.rand(size)  
     df  =  pd.DataFrame(data) # DataFrame作成
     df_ = df.to_dict() # DataFrameをシリアライズ可能なdictに変換
     return df_
+
+
+# オーケストレーション関数 からstartとDataFrame受け取る
+@app.activity_trigger(input_name="blank")
+def  activity2(context: df.DurableOrchestrationContext) -> float:
+    parameter = context.get_input()
+    start = int(parameter.get("start"))
+    transfer_time = time.time() - start
+    return transfer_time
