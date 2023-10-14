4313ad9bb539f09d22e755d2e749ef246bcb6e7a
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-10-15 00:38:31 +0900

    master

--- size-time/function_app.py
+++ size-time/function_app.py
@@ -1,4 +1,4 @@
-### データサイズをコードで設定し、応答速度を計測 ###
+### データサイズと転送方向をコードで設定し、応答速度を計測 ###
 import azure.functions as func
 import azure.durable_functions as df
 import logging
@@ -11,10 +11,13 @@ app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="orchestrators/client_function")
 @app.durable_client_input(client_name="client")
 async def client_function(req: func.HttpRequest, client: df.DurableOrchestrationClient) -> func.HttpResponse:
-    activity = int(req.params.get('activity') or req.get_json().get('activity'))
-    size = int(req.params.get('size') or req.get_json().get('size'))
+    try:
+        activity = int(req.params.get('activity') or req.get_json().get('activity'))
+        size = int(req.params.get('size') or req.get_json().get('size'))
+    except Exception as e:
+        return func.HttpResponse("Invalid 'activity' or 'size' parameters.", status_code=400)
 
-    instance_id = await client.start_new("orchestrator", None, {"activity":activity, "size":size})
+    instance_id = await client.start_new("orchestrator", None, {"activity": activity, "size": size})
     logging.info(f"Started orchestration with ID = '{instance_id}'.")
     
     # オーケストレーションの完了を待機
@@ -32,37 +35,29 @@ def  orchestrator(context: df.DurableOrchestrationContext):
     size = int(parameter.get("size"))
     
     if activity == 1:
-        result  =  yield context.call_activity("activity1", "size")
-        start = result("start")
-        transfer_time  =  time.time() - start
-        return  {"transfer_time" : transfer_time}
+        result  =  yield context.call_activity("activity1", size)
+        transfer_time  =  time.time() - result["start"]
+        return  {"Transfer_time from act1 to orc": transfer_time}
     
     elif activity == 2:
-        data = np.random.rand(size) 
-        df  =  pd.DataFrame(data) # DataFrame作成
-        df_ = df.to_dict() # DataFrameをシリアライズ可能なdictに変換
-        start  =  time.time()
-        result  =  yield context.call_activity("activity2", "start")
-        transfer_time = result("transfer_time")
-        return  {"transfer_time" : transfer_time}
+        data = np.random.rand(size) # データ作成
+        df  =  pd.DataFrame(data)
+
+        payload = {"df_": df.to_dict(), "start": time.time()}
+        transfer_time  =  yield context.call_activity("activity2", payload)
+        return  {"Transfer_time from orc to act2": transfer_time}
 
 
 # sizeからDataFrame作成し、アクティビティ関数 -> オーケストレーター関数に転送
-@app.activity_trigger(input_name="blank")
-def  activity1(context: df.DurableOrchestrationContext) -> str:
-    parameter = context.get_input()
-    size = int(parameter.get("size"))
-
+@app.activity_trigger(input_name="size")
+def  activity1(size: int) -> dict:
     data = np.random.rand(size)  
-    df  =  pd.DataFrame(data) # DataFrame作成
-    df_ = df.to_dict() # DataFrameをシリアライズ可能なdictに変換
-    return df_
+    df  =  pd.DataFrame(data)
+    return {"df_": df.to_dict(), "start": time.time()}
 
 
 # オーケストレーション関数 からstartとDataFrame受け取る
-@app.activity_trigger(input_name="blank")
-def  activity2(context: df.DurableOrchestrationContext) -> float:
-    parameter = context.get_input()
-    start = int(parameter.get("start"))
-    transfer_time = time.time() - start
-    return transfer_time
+@app.activity_trigger(input_name="payload")
+def  activity2(payload: dict) -> float:
+    start = payload["start"]
+    return time.time() - start
