236395549b420910c5e9855ec4ae0fa706094956
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-11-15 17:25:40 +0900

    master

--- origin-analysis/function_app.py
+++ origin-analysis/function_app.py
@@ -7,7 +7,6 @@ import azure.durable_functions as df
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -22,6 +21,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import linear_model
 import seaborn as sns
+import plotly.express as px
 
 import pickle
 import base64
@@ -45,6 +45,7 @@ def orchestrator(context: df.DurableOrchestrationContext) -> str:
 # 元のモノリシックコード
 @app.activity_trigger(input_name="blank")
 def origin_analysis(blank: str):
+### 探索的データ分析 ###
     # 出力を再現可能にするためにシードを設定
     np.random.seed(42)
     mpl.rc('axes', labelsize=14)
@@ -83,6 +84,7 @@ def origin_analysis(blank: str):
     housing.columns
     housing.head()
 
+### データの前処理 ###
     # sklearnは欠損データを処理できないため、欠損値を削除
     print(len(housing))
     print(housing.isnull().sum())
@@ -98,7 +100,7 @@ def origin_analysis(blank: str):
     print(X.shape)
     X.columns
 
-    # さらに、Xとyデータをトレーニングセットとテストセットに分割
+    # Xとyデータをトレーニングセットとテストセットに分割
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # 形状が予定通りになったことを確認するために形状を比較
@@ -110,19 +112,17 @@ def origin_analysis(blank: str):
     # データセットの数値変数
     X_train.describe().columns
 
-    # スケーラーの「インスタンス化」（sklearnクラスのインスタンスを作成）
-    std_scaler = StandardScaler()
-
     # X_trainデータにスケーラーを「fit」させる
+    std_scaler = StandardScaler()
     std_scaler = std_scaler.fit(X_train.values)
 
     # スケーラーを使用してデータセットを変換
     X_train_scaled = std_scaler.transform(X_train.values)
-    X_train_scaled[0]
     X_test_scaled = std_scaler.transform(X_test.values)
-    X_test_scaled[0]
 
-    # トレーニングデータセットにインスタンスを適合させる
+
+### 単回帰分析 ###
+    # 線形回帰モデルを作成
     lin_reg = LinearRegression(fit_intercept=True)
     lin_reg.fit(X_train_scaled, y_train)
 
@@ -130,108 +130,216 @@ def origin_analysis(blank: str):
     print(lin_reg.intercept_)
     print(lin_reg.coef_)
 
-    # 'Attributes'は特徴量のリストの別名です（予測子、独立変数）
-    attributes = X_test.columns
+    # 'Attributes' は、特徴（予測変数、独立変数）のリストの別名
+    attributes=X_test.columns
     print(attributes)
-    # 'Feature importances'は係数の別名です（各特徴量が結果またはDVに与える影響）
-    feature_importances = lin_reg.coef_
+
+    # 'Feature importances' は、係数の別名（つまり、各特徴が成果または DV に与える影響）
+    feature_importances=lin_reg.coef_
     print(feature_importances)
 
-    # 明らかに、これら2つのものは同じ長さを持っているでしょう
+    # 2つの要素は同じ長さ
     print(len(feature_importances))
     print(len(attributes))
 
-    [int(x) for x in list(feature_importances)]
+    feature_importances = [int(x) for x in feature_importances] # 係数を整数に変換します
 
-    # 結果を見てみましょう
+    # 結果
     feature_imp = pd.DataFrame(list(zip(attributes, feature_importances)), columns=['features', 'coeffs'])
-    feature_imp = feature_imp.set_index('features')
-    feature_imp = feature_imp.sort_values('coeffs')
+    feature_imp=feature_imp.set_index('features')
+    feature_imp=feature_imp.sort_values('coeffs')
 
-    # 棒グラフとしてプロット
-    feature_imp.plot(kind='bar')
-
-    # plotlyを使用して同じことを行う
+    # 棒グラフをプロット
+    feature_imp.plot(kind='bar');
     data = go.Bar(x=list(feature_imp.index), y=feature_imp['coeffs'])
     coefs = go.Figure([data])
-
-    X_train.columns
 
     # 単一の観測の変数を表示
     print(X_train.iloc[0])
 
-    # その単一の観測のスケーリングされた変数を表示
-    # '<1H OCEAN'変数を削除したことを覚えておいてください
+    # 単一の観測のスケールされた変数を表示
+    # 変数 "<1H OCEAN" を削除したことに注意
     print(X_train_scaled[0])
 
-    # その予測を行う
+    # 予測
     lin_reg.predict([X_train_scaled[0]])
 
-    # 似たようなフェイクデータを作成
+    # 似たような偽のデータを作成
     fake = np.array([-122, 37, 40, 2000, 3000, 500, 3, 3, 6, 4, 0, 0, 1, 0]).reshape(1, -1)
 
-    # トレーニングされたスケーラーを使用して標準化
+    # 訓練されたスケーラーを使用して標準化
     std_fake = std_scaler.transform(fake)
 
-    # その観測の予測を試してみる
+    # 予測を試す
     lin_reg.predict(std_fake)
 
-    # テストデータセット上で予測を行う
+    # テストデータセットの予測を行う
     y_preds = lin_reg.predict(X_test_scaled)
 
-    # 最初の5つの予測が最初の5つの実際の値とどのように比較されますか？
-    true_5 = list(round(y_test[:5], 1))
-    pred_5 = [round(x, 1) for x in list(y_preds[:5])]
+    # 最初の5つの予測と、最初の5つの実際の値を比較
+    true_5=list(round(y_test[:5], 1))
+    pred_5=[round(x,1) for x in list(y_preds[:5])]
     print('true values:', true_5)
     print('predicted values:', pred_5)
 
-    # これらの結果をどのように解釈しますか？
-    first_5 = ['district0', 'district1', 'district2', 'district3', 'distict4']
+    # 結果をどのように解釈するか
+    first_5=['district0', 'district1', 'district2', 'district3', 'distict4']
     pd.DataFrame(list(zip(first_5, true_5, pred_5)), columns=['district', 'true', 'predicted'])
 
-    # 平均二乗誤差はモデルの平均誤差（$で）を表します
+    # ルート平均二乗誤差（RMSE）は、モデルの平均誤差（ドル）を表します
     rmse_ols = np.sqrt(metrics.mean_squared_error(y_test, y_preds))
     rmse_ols = int(rmse_ols)
 
-    # これはコインフリップ（トレーニングセットの平均）と比較してどうなりますか？
-    avg_val = round(y_train.mean(), 2)
+    # コイン投げ（つまり、トレーニングセットの平均）と比較して、どれほど優れていますか？
+    avg_val = round(y_train.mean(),2)
 
-    # もしそれを予測子として使用した場合、モデルの平均誤差（RMSE）は次のとおりです。
-    coinflip_preds = np.full((len(y_test), ), avg_val)
-    rmse_coinflip = np.sqrt(metrics.mean_squared_error(y_test, coinflip_preds))
-    rmse_coinflip = int(rmse_coinflip)
+    # それを予測値として使用した場合、モデルの平均誤差（RMSE）
+    coinflip_preds=np.full((len(y_test), ), avg_val)
+    rmse_coinflip=np.sqrt(metrics.mean_squared_error(y_test, coinflip_preds))
+    rmse_coinflip=int(rmse_coinflip)
 
-    # R-squaredはモデルによって説明される従属変数の分散の割合です
-    r2_ols = metrics.r2_score(y_test, y_preds)
-    r2_ols = round(r2_ols, 2)
+    # R-squaredは、DVの分散のうちモデルによって説明される割合
+    r2_ols=metrics.r2_score(y_test, y_preds)
+    r2_ols=round(r2_ols, 2)
 
-    # これはコインフリップ（トレーニングセットの平均）と比較してどうなりますか？
-    r2_coinflip = metrics.r2_score(y_test, coinflip_preds)
-    r2_coinflip = round(r2_coinflip, 2)
+    # コイン投げ（つまり、トレーニングセットの平均）と比較して、どれほど優れていますか？
+    r2_coinflip=metrics.r2_score(y_test, coinflip_preds)
+    r2_coinflip=round(r2_coinflip,2)
 
     # OLS線形回帰をベースラインと比較
-    evaluation_df = pd.DataFrame([['ベースライン', rmse_coinflip, r2_coinflip],
-                                  ['OLS線形回帰', rmse_ols, r2_ols]],
-                                 columns=['モデル', 'RMSE', 'R-squared']
-                                 )
-    evaluation_df.set_index('モデル', inplace=True)
+    evaluation_df = pd.DataFrame([['Baseline',rmse_coinflip, r2_coinflip],
+                                      ['OLS Linear Regression', rmse_ols, r2_ols]],
+                                     columns=['Model','RMSE','R-squared']
+                                    )
+    evaluation_df.set_index('Model', inplace=True)
 
-    # Bar chart with plotly: RMSE
+    # RMSE: Plotlyを使用した棒グラフ
     trace = go.Bar(x=list(evaluation_df.index), y=evaluation_df['RMSE'], marker=dict(color=['#E53712', '#1247E5']))
-    layout = go.Layout(title='地区別の中央住宅価格：平均二乗平方根誤差',  # グラフのタイトル
-                       yaxis=dict(title='モデル'),  # x軸のラベル
-                       xaxis=dict(title='RMSE'),  # y軸のラベル
-                       )
+    layout = go.Layout(title = '地区ごとの平均家屋価値：ルート平均二乗誤差', # グラフのタイトル
+            yaxis = dict(title = 'モデル'), # x軸ラベル
+            xaxis = dict(title = 'RMSE'), # y軸ラベル
+                          )
 
-    rmse_fig = go.Figure(data=[trace], layout=layout)
+    fig = go.Figure(data = [trace], layout=layout)
 
-    # Bar chart with plotly: R-Squared
+    # R-squared: Plotlyを使用した棒グラフ
     trace = go.Bar(x=list(evaluation_df.index), y=evaluation_df['R-squared'], marker=dict(color=['#E53712', '#1247E5']))
-    layout = go.Layout(title='地区別の中央住宅価格: R-squared',  # グラフのタイトル
-                       yaxis=dict(title='モデル'),  # x軸のラベル
-                       xaxis=dict(title='R-Squared'),  # y軸のラベル
-                       )
+    layout = go.Layout(title = '地区ごとの平均家屋価値：R-squared', # グラフのタイトル
+            yaxis = dict(title = 'モデル'), # x軸ラベル
+            xaxis = dict(title = 'R-Squared'), # y軸ラベル
+                          )
 
-    r2_fig = go.Figure(data=[trace], layout=layout)
+    fig = go.Figure(data = [trace], layout=layout)
 
-    return 0
+    # データ可視化
+    fig = sns.regplot(x=y_preds, y=y_test)
+
+    # Plotlyでも可視化
+    fig = px.scatter(y=y_test, x=y_preds, trendline="ols", width=500, height=500)
+    fig.update_layout(title='地区別の住宅の中間値',  # グラフタイトル
+                      yaxis=dict(title='真の値'),  # x軸ラベル
+                      xaxis=dict(title='予測値'),  # y軸ラベル
+                      )
+    fig.update_traces(line_color='#E53712', line_width=5)
+    fig.show()
+
+
+### Ridge回帰モデル ###
+    # Ridge回帰モデルを訓練データに当てはめる
+    ridge_model = linear_model.Ridge(alpha=.5)
+    ridge_model.fit(X_train_scaled, y_train)
+
+    # テストデータに対する予測
+    y_preds = ridge_model.predict(X_test_scaled)
+
+    # 平均二乗誤差（RMSE）は、モデルの平均誤差（ドル単位）を表す
+    ridge_rmse = int(np.sqrt(metrics.mean_squared_error(y_test, y_preds)))
+
+    # R2スコアは、モデルによって説明されるDVの分散の割合です
+    ridge_r2 = round(metrics.r2_score(y_test, y_preds), 2)
+    print(ridge_rmse, ridge_r2)
+
+
+### K近傍法モデル ###
+    # K近傍法モデルを訓練データに当てはめる
+    knn_model = KNeighborsRegressor(n_neighbors=8)
+    knn_model.fit(X_train_scaled, y_train)
+
+    # テストデータに対する予測
+    y_preds = knn_model.predict(X_test_scaled)
+
+    # 平均二乗誤差（RMSE）は、モデルの平均誤差（ドル単位）を表す
+    knn_rmse = int(np.sqrt(metrics.mean_squared_error(y_test, y_preds)))
+
+    # R2スコアは、モデルによって説明されるDVの分散の割合
+    knn_r2 = round(metrics.r2_score(y_test, y_preds), 2)
+    print(knn_rmse, knn_r2)
+
+
+### 決定木回帰モデル ###
+    # 決定木回帰モデルを訓練データに当てはめる
+    tree_model = DecisionTreeRegressor(max_depth=9)
+    tree_model.fit(X_train_scaled, y_train)
+
+    # テストデータに対する予測
+    y_preds = tree_model.predict(X_test_scaled)
+
+    # 平均二乗誤差（RMSE）は、モデルの平均誤差（ドル単位）を表す
+    tree_rmse = int(np.sqrt(metrics.mean_squared_error(y_test, y_preds)))
+
+    # R2スコアは、モデルによって説明されるDVの分散の割合
+    tree_r2 = round(metrics.r2_score(y_test, y_preds), 2)
+    print(tree_rmse, tree_r2)
+
+
+### random forestモデル ###
+    # random forestモデルを訓練データに当てはめる
+    forest_model = RandomForestRegressor(max_depth=10, n_estimators=200)
+    forest_model.fit(X_train_scaled, y_train)
+
+    # テストデータセットで予測
+    y_preds = forest_model.predict(X_test_scaled)
+
+    # ルート平均二乗誤差（RMSE）は、モデルの平均誤差（ドル単位）を表す
+    forest_rmse = int(np.sqrt(metrics.mean_squared_error(y_test, y_preds)))
+
+    # R2スコアは、説明変数によって説明される従属変数の分散の割合
+    forest_r2=round(metrics.r2_score(y_test, y_preds),2)
+
+    print(forest_rmse, forest_r2)
+
+
+### 棒グラフで結果可視化 ###
+    evaluation_df2 = pd.DataFrame([['ベースライン',rmse_coinflip, r2_coinflip],
+                                      ['OLS線形回帰', rmse_ols, r2_ols],
+                                      ['リッジ回帰', ridge_rmse, ridge_r2],
+                                      ['K近傍法回帰', knn_rmse, knn_r2],
+                                      ['決定木回帰', tree_rmse, tree_r2],
+                                      ['ランダムフォレスト回帰', forest_rmse, forest_r2]],
+                                     columns=['モデル','RMSE','R-squared']
+                                    )
+    evaluation_df2.set_index('モデル', inplace=True)
+
+    # RMSEの棒グラフを作成
+    trace = go.Bar(x=list(evaluation_df2.index),
+                       y=evaluation_df2['RMSE'],
+                       marker=dict(color=['gray', '#e96060', 'gray', 'gray', 'gray', 'gray']),
+                      )
+    layout = go.Layout(title = 'モデル比較: ルート平均二乗誤差', # グラフタイトル
+            yaxis = dict(title = 'モデル'), # x軸ラベル
+            xaxis = dict(title = 'RMSE'), # y軸ラベル
+                          )
+
+    rmse_fig = go.Figure(data = [trace], layout=layout)
+
+    # R2スコアの棒グラフを作成
+    trace = go.Bar(x=list(evaluation_df2.index),
+                       y=evaluation_df2['R-squared'],
+                       marker=dict(color=['gray', '#e96060', 'gray', 'gray', 'gray', 'gray']),
+                      )
+    layout = go.Layout(title = 'モデル比較: R-Squared', # グラフタイトル
+            yaxis = dict(title = 'モデル'), # x軸ラベル
+            xaxis = dict(title = 'R-Squared'), # y軸ラベル
+                          )
+
+    r2_fig = go.Figure(data = [trace], layout=layout)
