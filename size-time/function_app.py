8b427ddc0b5d097528cc9f8c1cad05f2b23488c6
J0KERtry <99167531+J0KERtry@users.noreply.github.com>
2023-11-23 12:10:28 +0900

    master

--- size-time/function_app.py
+++ size-time/function_app.py
@@ -3,7 +3,8 @@ import azure.functions as func
 import azure.durable_functions as df
 import numpy as np
 import pandas as pd
-import sys
+import pickle
+import base64
 
 app = df.DFApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="orchestrators/client_function")
@@ -28,39 +29,16 @@ def orchestrator(context: df.DurableOrchestrationContext) -> dict:
     custom_properties = {"actibity": activity, "size": size }
     context.set_custom_status(custom_properties)
     
-    if activity == 1: #DataFrame   
-        result  =  yield context.call_activity("activity1", size)
-        data_frame = pd.DataFrame.from_dict(result["data"])  # デシリアライズ
-    
-    elif activity == 2: # Numpy配列
+    if activity == 1: # Numpy配列
         result  =  yield context.call_activity("activity2", size)
-        receive = np.array(result["data"])
-    
-    elif activity == 3: # list
-        result  =  yield context.call_activity("activity3", size)
-        receive = result["data"]
+        data = pickle.loads(base64.b64decode(result["data"]))
 
     return 'orchestrator end'
 
 
-# DataFrameを辞書型にして転送
-@app.activity_trigger(input_name="size")
-def  activity1(size: int) -> dict:
-    data = np.random.rand(size) # 1行size列のNumpy配列作成
-    data  =  pd.DataFrame(data) # Numpy配列をDataframeに変換
-    data_ = data.to_dict() # シリアライズ可能な型に変換
-    return {"data": data_}
-
 # Numpy配列を作成し転送
 @app.activity_trigger(input_name="size")
-def  activity2(size: int) -> dict:
-    data = np.random.randint(0, 100, size=size*250000, dtype=np.int32)
-    data = data.tolist()
-    return {"data": data}
-
-# リスト型を作成し転送
-@app.activity_trigger(input_name="size")
-def  activity3(size: int) -> dict:
-    size = size * 1024 * 1024
-    data = [0] * (size // sys.getsizeof(0))
+def  activity1(size: int) -> dict:
+    data = np.random.randint(0, 100, size= 196610 * size, dtype=np.int32)
+    data = base64.b64encode(pickle.dumps(data)).decode()
     return {"data": data}
